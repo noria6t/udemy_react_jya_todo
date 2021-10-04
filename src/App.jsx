cb4510ea0759902971b6f3c621e15450d65aa638
noria_t <55936478+noria6t@users.noreply.github.com>
2021-10-04 19:47:17 +0900

    useState が変わると再レンダリングする。 on/offボタン

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
+// useStateが変わると上から順に読み込む。これを再レンダリングという。
 const App = () => {
+  // [num, setNum]はnumが変数、第二引数setNumはstateを変更するための関数。0は初期値。
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
   const onClickCountUp = () => {
     // num + 1のところはnumを更新したい値を入れる部分。
     setNum(num + 1);
   };
-  // [num, setNum]はnumが変数、第二引数setNumはstateを変更するための関数。0は初期値。
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは!</h1>
       <ColorfulMessage color="blue">お元気ですか?</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ！</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>( ´∀｀)</p>}
     </>
   );
 };
