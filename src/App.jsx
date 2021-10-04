065d5ff59b784e64846f93b309ab91f12ab25c26
noria_t <55936478+noria6t@users.noreply.github.com>
2021-10-04 19:35:10 +0900

    useState追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   const onClickButton = () => alert();
+  // [num, setNum]はnumが変数、第二引数setNumはstateを変更するための関数。0は初期値。
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは!</h1>
       <ColorfulMessage color="blue">お元気ですか?</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 };
