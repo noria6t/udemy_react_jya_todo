af7977394814994b7e23b67d8da4a95b04c5ada8
noria_t <55936478+noria6t@users.noreply.github.com>
2021-10-04 19:22:50 +0900

    props使い方１

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ const App = () => {
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは!</h1>
-      <ColorfulMessage />
-      {/* <p style={contentStyle}>元気です!</p> */}
+      <ColorfulMessage color="blue" message="お元気ですか？" />
+      <ColorfulMessage color="pink" message="元気です!" />
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
